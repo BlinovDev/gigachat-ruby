81a75dc3d76d31a866469a7b44ec8c2150385945
Anton <anton253647@gmail.com>
2024-09-22 02:10:20 +0200

    Added bootstrap

--- Gemfile
+++ Gemfile
@@ -32,8 +32,9 @@ gem "tzinfo-data", platforms: %i[ windows jruby ]
 gem "bootsnap", require: false
 
 
-# Gemfile
 gem 'bcrypt', '~> 3.1.7'
+
+gem 'sassc-rails'
 
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+// Import Bootstrap and its dependencies
+import "bootstrap"
+import "@popperjs/core"
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "popper"
+import "controllers"
